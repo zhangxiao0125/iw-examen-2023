2f300dd649f474a3b9b6459630cb9e866ff19744
zhangxiao0125 <zhangxiao@uma.es>
2023-01-18 17:58:11 +0100

    delete mi-perfil and owner

--- pages/api/housings/owner/[nombre].js
+++ pages/api/housings/owner/[nombre].js
@@ -7,17 +7,17 @@ connectMongoDB()
 export default async (req, res) => {
 	const {
 		method,
-		query: {owner},
+		query: {nombre},
 	} = req;
 
 	switch(method){		
 		case 'GET':
 			try{
-                const user = await User.find({username: new RegExp(owner, 'i')})
-				if(!user) return res.status(404).json({msg: "User not found"})
+                const user = await User.find({username: new RegExp(nombre, 'i')})
+				if(!user) return res.status(404).json({msg: "parking not found"})
 				var firstUser = ((user[0]["_id"]).valueOf())
 
-				const housing = await Housing.find({owner: firstUser})
+				const housing = await Housing.find({nombre: firstUser})
 				if(!housing) return res.status(404).json({msg: "Housing not found"})
 				return res.status(200).json(housing)
 
