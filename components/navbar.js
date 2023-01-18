5f64f755b5cce215ea52e5609a17fce2c0e5233f
zhangxiao0125 <zhangxiao@uma.es>
2023-01-18 17:06:54 +0100

    vario views

--- components/navbar.js
+++ components/navbar.js
@@ -13,8 +13,8 @@ const NavbarItem = () => {
 	return (
 		<Navbar fluid={true} rounded={true}>
 			<Navbar.Brand href={`http://${process.env.URL}/housings`}>
-				<img src="logo-blue-3.png" className="mr-3 h-6 sm:h-9" alt="iHOME"/>
-				<span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">iHOME</span>
+				<img src="ParkingNet.jpg" className="mr-3 h-6 sm:h-9" alt="iHOME"/>
+				<span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">HOME</span>
 			</Navbar.Brand>
 			
 			{(pathIndex) ? (
@@ -24,7 +24,7 @@ const NavbarItem = () => {
 					<Navbar.Link href="/housings" active={path == '/housings'}>Alojamientos</Navbar.Link>
 					<Navbar.Link href="/users" active={path == '/users'}>Usuarios</Navbar.Link>
 					<Navbar.Link href="/bookings" active={path == '/bookings'}>Reservas</Navbar.Link>
-					<Navbar.Link href="/madrid" active={path == '/madrid'} className="underline">Madrid</Navbar.Link>
+					<Navbar.Link href="/mi-perfil" active={path == '/mi-perfil'} className="underline">Mi Perfil</Navbar.Link>
 				</Navbar.Collapse>
 			</div>)}
 
@@ -39,10 +39,10 @@ const NavbarItem = () => {
 							<Button onClick={()=> signIn()} className="bg-blue-600 hover:bg-blue-800">Iniciar sesión</Button>
 							<Navbar.Toggle/>
 						</div>
-						<div className="px-2">
+						{/* <div className="px-2">
 							<Link href='new-user'><Button className="bg-slate-600 hover:bg-slate-900">Registrarse</Button></Link>
 							<Navbar.Toggle/>
-						</div>
+						</div> */}
 					</div>
 				)}
 			
